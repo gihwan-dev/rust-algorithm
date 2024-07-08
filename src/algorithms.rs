9583b7dfe45cfba0b0fb2dd48930a81a0a55ba65
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-07-08 14:54:43 +0900

    .

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,1 +1,5 @@
 pub mod zig_zag_conversion;
+
+pub mod find_index_of_the_first_occurrence_in_a_string;
+
+pub mod text_justification;
--- src/algorithms/zig_zag_conversion.rs
+++ src/algorithms/zig_zag_conversion.rs
@@ -1,7 +1,26 @@
 struct Solution {}
 
 impl Solution {
-  pub fn convert(s: String ,num_rows: i32) -> String {
-    String::from("Hello World")
-  }
+    pub fn convert(s: String, num_rows: i32) -> String {
+        let mut zigzags: Vec<_> = (0..num_rows)
+            .chain((1..num_rows - 1).rev())
+            .cycle()
+            .zip(s.chars())
+            .collect();
+        zigzags.sort_by_key(|&(row, _)| row);
+        zigzags.into_iter().map(|(_, c)| c).collect()
+    }
+}
+
+#[test]
+fn test_zig_zag_conversion() {
+    let s = String::from("PAYPALISHIRING");
+    let nums_rows = 3;
+    let result = Solution::convert(s, nums_rows);
+    assert_eq!(result, String::from("PAHNAPLSIIGYIR"));
+
+    let s = String::from("PAYPALISHIRING");
+    let nums_rows = 4;
+    let result = Solution::convert(s, nums_rows);
+    assert_eq!(result, String::from("PINALSIGYAHRPI"));
 }
